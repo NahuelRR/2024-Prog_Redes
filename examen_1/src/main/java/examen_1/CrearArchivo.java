5b73609ade27d73552802c46c8de330ef721d6ec
NahuelRR <101563071+NahuelRR@users.noreply.github.com>
2024-07-10 13:12:56 -0300

    commit

--- examen_1/src/main/java/examen_1/CrearArchivo.java
+++ examen_1/src/main/java/examen_1/CrearArchivo.java
@@ -1,5 +1,21 @@
 package examen_1;
 
-public class CrearArchivo {
+import java.io.File;
+import java.io.IOException;
 
+public class CrearArchivo {
+    public static void main(String[] args) {
+        try {
+            File ficheroDatos = new File("datos.txt");            
+            if (ficheroDatos.createNewFile()) {
+                System.out.println("Fichero "+ ficheroDatos.getName() +" creado");
+            }            
+            else {
+                System.out.println("No se ha podido crear el fichero. Probablemente ya exista.");
+            }
+        }catch(IOException error){
+            System.out.println("Error al crear el fichero.");
+            error.printStackTrace();
+        }
+    }
 }
